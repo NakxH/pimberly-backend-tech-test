e2f5192f323139818266247efbdce5064daa841f
Hakan <74737999+NakxH@users.noreply.github.com>
2022-03-10 19:47:18 +0000

    added abilty to read csv file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const csvParser = require('csv-parser');
+const fs = require('fs');
+const csvFilePath = '/files/products.csv'
 
 const app = express();
 
@@ -7,6 +10,12 @@ mongoose
     .connect("mongodb://localhost:27017/pimberly", { useNewUrlParser: true })
     .then(() => {
         const app = express()
+        fs.createReadStream(__dirname + csvFilePath)
+            .pipe(csvParser({}))
+            .on('data', (data) => {
+                console.log(data)
+            })
+
         app.listen(3000, () => {
             console.log("Server has started!")
         })
