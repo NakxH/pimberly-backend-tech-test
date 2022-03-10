4420a58d490f2dc226abaaa33184f900695fb7cc
Hakan <74737999+NakxH@users.noreply.github.com>
2022-03-10 22:29:35 +0000

    added code comments + abilty to run handleFile with a specified path

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const csvParser = require("csv-parser");
 const fs = require("fs");
 const csvFilePath = "/files/products.csv";
 const Product = require("./models/Product");
 
-const app = express();
-
-async function handleFile() {
+/*
+  Function that imports a csv file with headers from a specified path
+  
+  If all columns contain data and there are no existing products with the same sku in the db
+  the product will be saved to the db
+  
+  If there is an existing product in the db with the same sku that has updated product data,
+  it will be updated.
+*/
+async function handleFile(path) {
   try {
-    const file = await fs.promises.readFile(__dirname + csvFilePath);
+    const count = {
+      created: 0,
+      updated: 0,
+      unchanged: 0,
+      skipped: 0,
+    };
+    const file = await fs.promises.readFile(__dirname + path);
     const fileContents = file.toString();
     const rows = fileContents.split("\n");
 
@@ -19,40 +31,70 @@ async function handleFile() {
 
         if (cols[0] === "") {
           console.log(`missing sku @ ${i}`);
+          count.skipped++;
           continue;
         }
         if (cols[1] === "") {
           console.log(`missing colour @ ${i}`);
+          count.skipped++;
           continue;
         }
         if (cols[2] === "") {
           console.log(`missing size @ ${i}`);
+          count.skipped++;
           continue;
         }
 
         const foundProduct = await Product.findOne({ sku: cols[0] });
-
-        if (foundProduct === null) {
+        if (foundProduct) {
+          if (
+            foundProduct.colour !== cols[1] ||
+            foundProduct.size !== cols[2]
+          ) {
+            foundProduct.colour = cols[1];
+            foundProduct.size = cols[2];
+            await foundProduct.save();
+            count.updated++;
+          } else {
+            count.unchanged++;
+          }
+        } else if (foundProduct === null) {
           const product = new Product({
             sku: cols[0],
             colour: cols[1],
             size: cols[2],
           });
           await product.save();
+          count.created++;
         }
       }
     }
+
+    console.log(`Number of rows imported: ${rows.length - 1}`);
+    console.log(`Number of products created: ${count.created}`);
+    console.log(`Number of products updated: ${count.updated}`);
+    console.log(`Number of products unchanged: ${count.unchanged}`);
+    console.log(`Number of rows skipped: ${count.skipped}`);
   } catch (err) {
     console.error(err);
   }
 }
 
+/*
+  Connect to databse and start express app
+*/
 mongoose
   .connect("mongodb://localhost:27017/pimberly", { useNewUrlParser: true })
   .then(async () => {
     const app = express();
 
-    await handleFile();
+    await handleFile(csvFilePath);
+
+    // Get request to get all products from database
+    app.get("/", async (req, res) => {
+      const products = await Product.find();
+      res.send(products);
+    });
 
     app.listen(3000, () => {
       console.log("Server has started!");
